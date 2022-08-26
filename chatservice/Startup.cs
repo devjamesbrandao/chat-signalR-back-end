d55e4cb40391943bf8986037b751971818ab08a1
James Noleto <muricijames@gmail.com>
2022-08-25 22:33:10 -0300

    feat: add configuração do SignalR

---
+++ chatservice/Startup.cs
@@ -0,0 +1,45 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using SignalRChat.Hubs;
+using System.Collections.Generic;
+
+namespace SignalRChat
+{
+    public class Startup
+    {
+        public static IConfiguration Configuration { get; set; }
+        
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSignalR();
+
+            services.AddCors(options =>
+            {
+                options.AddDefaultPolicy(builder =>
+                    {
+                        builder.WithOrigins(Configuration.GetValue<string>("ORIGIN_URL_CORS"))
+                            .AllowAnyHeader()
+                            .AllowAnyMethod()
+                            .AllowCredentials();
+                    });
+            });
+
+            services.AddSingleton<IDictionary<string, UserConnection>>(opts => new Dictionary<string, UserConnection>());
+        }
+
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment()) app.UseDeveloperExceptionPage();
+
+            app.UseRouting();
+
+            app.UseCors();
+
+            app.UseEndpoints(endpoints => { endpoints.MapHub<ChatHub>("/chat"); });
+        }
+    }
+}
