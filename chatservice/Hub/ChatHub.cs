eda39e5b07ad7322571918c2e96eb45d6d76d8c3
James Noleto <muricijames@gmail.com>
2022-08-25 22:35:34 -0300

    feat: hub do signalR responsável para conectar usuários no chat, alertar usuários, enviar mensagens, etc

---
+++ chatservice/Hub/ChatHub.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+
+namespace SignalRChat.Hubs
+{
+    public class ChatHub : Hub
+    {
+        private readonly string _automaticMessage;
+
+        private readonly IDictionary<string, UserConnection> _connections;
+
+        public ChatHub(IDictionary<string, UserConnection> connections)
+        {
+            _automaticMessage = "Mensagem automática"; 
+
+            _connections = connections;
+        }
+
+        public override Task OnDisconnectedAsync(Exception exception)
+        {
+            if (_connections.TryGetValue(Context.ConnectionId, out UserConnection userConnection))
+            {
+                _connections.Remove(Context.ConnectionId);
+
+                Clients.Group(userConnection.Room).SendAsync("ReceiveMessage", _automaticMessage, $"{userConnection.User} has left");
+
+                SendUsersConnected(userConnection.Room);
+            }
+
+            return base.OnDisconnectedAsync(exception);
+        }
+
+        public async Task JoinRoom(UserConnection userConnection)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, userConnection.Room);
+
+            _connections[Context.ConnectionId] = userConnection;
+
+            await Clients.Group(userConnection.Room).SendAsync("ReceiveMessage", _automaticMessage, $"{userConnection.User} has joined {userConnection.Room}");
+
+            await SendUsersConnected(userConnection.Room);
+        }
+
+        public async Task SendMessage(string message)
+        {
+            if (_connections.TryGetValue(Context.ConnectionId, out UserConnection userConnection))
+            {
+                await Clients.Group(userConnection.Room).SendAsync("ReceiveMessage", userConnection.User, message);
+            }
+        }
+
+        public Task SendUsersConnected(string room)
+        {
+            var users = _connections.Values
+                                    .Where(c => c.Room == room)
+                                    .Select(c => c.User);
+
+            return Clients.Group(room).SendAsync("UsersInRoom", users);
+        }
+    }
+}
