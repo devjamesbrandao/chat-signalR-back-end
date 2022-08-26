6e91c369f56fb06f1c0487ae8823e777f229211a
James Noleto <muricijames@gmail.com>
2022-08-25 22:34:46 -0300

    feat: entidade para o usuario conectado

---
+++ chatservice/UserConnection.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace SignalRChat
+{
+    public class UserConnection
+    {
+        public string Id { get; } = Guid.NewGuid().ToString();
+        public string User { get; set; }
+        public string Room { get; set; }
+    }
+}
